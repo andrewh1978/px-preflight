8a42190168d06cf817852600d0b995b46bd39522
Andrew Hill <andrew@portworx.com>
2022-01-21 17:39:05 +0000

    parallelise scp

--- distribute_image.sh
+++ distribute_image.sh
@@ -1,6 +1,6 @@
 docker save px-preflight -o px-preflight.tar
 
-for i in $(kubectl get nodes -o jsonpath='{.items[*].metadata.name}'); do
-  scp px-preflight.tar $i:/var/tmp
-  ssh $i docker load -i /var/tmp/px-preflight.tar
+for i in $(kubectl get nodes -lnode-role.kubernetes.io/master!="",px/enabled!=false -o jsonpath='{.items[*].metadata.name}'); do
+  (scp px-preflight.tar $i:/var/tmp && ssh $i docker load -i /var/tmp/px-preflight.tar) &
 done
+wait
